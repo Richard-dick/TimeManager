6fad388c13003fe7a59bb8268dd19a5a6ff49a8c
lzh <liziheng20@mails.ucas.ac.cn>
2023-08-17 22:16:07 +0800

    second day in exploring: bad

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,25 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    // ! 时间显示
+    timerUpdate();
+    QTimer *timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(timerUpdate()));
+    timer->start(1000);
+
+    // ! 初始化 任务 Table
+    // 设置列标题
+    ui->missionWorkingTable->setColumnCount(ColumnCount);
+    QStringList headerLabels;
+    headerLabels << "任务简称" << "起始时间" << "截止日期";
+    ui->missionWorkingTable->setHorizontalHeaderLabels(headerLabels);
+
+    // 添加示例数据，这只是一个简化的示例
+    // ui->missionWorkingTable->insertRow(0);
+    // ui->missionWorkingTable->setItem(0, 0, new QTableWidgetItem("任务1"));
+    // ui->missionWorkingTable->setItem(0, 1, new QTableWidgetItem("2023-08-15"));
+    // ui->missionWorkingTable->setItem(0, 2, new QTableWidgetItem("2023-08-20"));
+
     // ! 添加槽函数
     connect(ui->addButton, &QPushButton::clicked, this, &MainWindow::add_mission);
 }
@@ -28,9 +47,19 @@ void MainWindow::add_mission()
         QString gender = dialog.getGender();
 
         // 将信息添加到表格
-        int row = ui->tableWidget->rowCount();
-        ui->tableWidget->insertRow(row);
-        ui->tableWidget->setItem(row, 0, new QTableWidgetItem(name));
-        ui->tableWidget->setItem(row, 1, new QTableWidgetItem(gender));
+        int row = ui->missionWorkingTable->rowCount();
+        ui->missionWorkingTable->insertRow(row);
+        ui->missionWorkingTable->setItem(row, 0, new QTableWidgetItem(name));
+        ui->missionWorkingTable->setItem(row, 1, new QTableWidgetItem(gender));
     }
+}
+
+// 定义成员函数timerUpdate()实现用户界面显示时间：
+void MainWindow::timerUpdate()
+{
+    currentTime = QDateTime::currentDateTime();
+
+    QString str = currentTime.toString("yyyy-MM-dd \ndddd \nhh:mm:ss ");
+
+    ui->datetimeLabel->setText(str);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,12 +4,16 @@
 #include <QMainWindow>
 #include <QPushButton>
 #include <QTableWidgetItem>
+#include <QDateTime>
+#include <QTimer>
 
 #include "addDialog.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+const int ColumnCount = 3; // missionXXTable的列数
 
 class MainWindow : public QMainWindow
 {
@@ -21,10 +25,12 @@ public:
 
 private:
     Ui::MainWindow *ui;
-    QTableWidget *tableWidget; // 声明一个成员变量以便在槽函数中使用
+    QDateTime currentTime;
+    // QTableWidget *tableWidget; // 声明一个成员变量以便在槽函数中使用
 
 
 private slots:
     void add_mission();
+    void timerUpdate();
 };
 #endif // MAINWINDOW_H
